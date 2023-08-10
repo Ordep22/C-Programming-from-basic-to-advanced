0bde870f9fbe404e4c61d2ba7d6212cb1dab7436
Pedro Vitor Pereira <pedro.pereira@lupatecnologia.com.br>
2023-08-10 14:22:30 -0300

    Lesson #Ifndef finish

---
+++ Section_14 -  Build Directive/ifndef.C
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+
+#ifndef PI
+
+    #define PI 3.14567
+
+#endif
+
+
+int main(){
+
+    int number = 5;
+
+    number  = 467;
+
+    printf("Number: %d\n", number);
+
+    printf("Pi: %f\n",PI);
+
+}
