64db15353d7ff3d6ce1f233625e2a3e9c772afc0
pedrovitorcomercial@gmail.com <pedrovitorcomercial@gmail.com>
2023-09-29 14:53:22 -0300

    Start lesson 93. Queue

--- Section_17 - Data Structures in C/Queue.C
+++ Section_17 - Data Structures in C/Queue.C
@@ -1,9 +1,53 @@
  /*
- Queue in C
+Queue in C
 
+FIFO - First in First Out 
 
- 
- 
- 
- 
+enqueu() - Add an element on a queue
+
+dequeue() - Remove an elemenot from a queue
+
+clean() - Clean the queue
+
+Queue Head [_ _ _E_L_E_N_T_S_ _ _] tail
+
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+#define LEMQUEUE 10
+uint8_t queue[LEMQUEUE] = {0};
+uint8_t head = 0;
+uint8_t tail = 0;
+
+void Queue(){
+
+    printf("\n----------------------- Queue -----------------------\n");
+    printf("[");
+    for (int i = 0;i < LEMQUEUE; i++){
+
+            printf("%d",queue[i]);
+            if(i != (LEMQUEUE-1)){
+
+                printf(", ");
+
+
+            }
+            
+    }
+
+    printf("]");
+
+
+}
+
+
+
+int main(){
+
+    Queue();
+
+}
