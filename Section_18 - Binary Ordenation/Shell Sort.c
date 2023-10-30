1889681fcb2945ac854c2d13d8437eb66d6abbe6
unknown <pedrovitorcomercial@gmail.com>
2023-10-29 22:29:24 -0300

    Fisnih Lesson 107. Sheel Sort

--- Section_18 - Binary Ordenation/Shell Sort.c
+++ Section_18 - Binary Ordenation/Shell Sort.c
@@ -9,15 +9,43 @@
 void ShellSort(int vector[], int size){
 
     int groupe = ONE;
+    int change;
+    int j;
 
     //Generate the size of the goupe form the size of vector
     while (groupe < size){
 
         groupe  = 3 * groupe + 1;
-        
+
 
     }
 
+    //Walking the vector while there is groupe to ordenate
+    while(groupe > 1 ){
+
+        //Divede the goupe in three parts
+
+        groupe /= 3;
+
+        //Walking in to this groupes
+        for (int i = groupe; i < size; i++ ){
+
+            change = vector[i];
+            j = i - groupe;
+
+            //Realize the change
+            while (j >= 0 && change < vector[j]){
+
+                vector[j + groupe] = vector[j];
+                j -= groupe;
+
+            }
+            vector[j + groupe] = change;
+
+        }
+
+
+    }
 
 
 
